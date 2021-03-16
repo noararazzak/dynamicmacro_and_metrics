c160af905b09d1747d54fe250f3a2a3d4d9d313f
Noara Razzak <Noara.Razzak@gmail.com>
2021-03-17 03:40:43 +0600

    Added main.py

---
+++ ECON8050/main.py
@@ -0,0 +1,12 @@
+import macro_ps9_1
+
+
+def main():
+    answer = macro_ps9_1.calculations_agg(.25, 1, 5, 10, .33, 0.05)
+    print(answer)
+    #answer = macro_ps8_2.maxutility2(1, 1, 3, 0.25, 0.98, 2, 1.6)
+    #print(answer)
+
+
+if __name__ == "__main__":
+    main()
